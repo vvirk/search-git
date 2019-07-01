8b1e28b64ae20fce71425460dc0fb91fdc05a55e
Boxing199 <virkolomeo@gmail.com>
2019-07-01 12:44:43 +0300

    Updates

--- precache-manifest.0c0d42ad2e2e9ea49b55cb639d15db1b.js
+++ precache-manifest.0c0d42ad2e2e9ea49b55cb639d15db1b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db31a84dfb692f8c02b2d61667f2b6b0",
+    "revision": "dad7d028e82e9c15e606e6be93e8c4a7",
     "url": "/search-git/index.html"
   },
   {
-    "revision": "74933edbd44c13f0fabd",
+    "revision": "5765bb5398eb14f39c0f",
     "url": "/search-git/static/css/main.e5747302.chunk.css"
   },
   {
-    "revision": "2567187199916eebe1ec",
-    "url": "/search-git/static/js/2.2b69251b.chunk.js"
+    "revision": "e1c6baeb1c6c59f8e52b",
+    "url": "/search-git/static/js/2.14dce1fc.chunk.js"
   },
   {
-    "revision": "74933edbd44c13f0fabd",
-    "url": "/search-git/static/js/main.24c8899b.chunk.js"
+    "revision": "5765bb5398eb14f39c0f",
+    "url": "/search-git/static/js/main.48c16cb5.chunk.js"
   },
   {
     "revision": "6d7065b89f39f4d8f42a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/search-git/precache-manifest.6f785a43352c2e2448d1897a0142acc8.js"
+  "/search-git/precache-manifest.0c0d42ad2e2e9ea49b55cb639d15db1b.js"
 );
 
 self.addEventListener('message', (event) => {
